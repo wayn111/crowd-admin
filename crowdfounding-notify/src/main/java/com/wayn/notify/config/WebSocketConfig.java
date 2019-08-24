9c2184693686df4181be281bb6c124abef4153f4
wayn <1669738430@qq.com>
2019-08-24 17:37:30 +0800

    优化页面按钮显示文字，添加个人资料页面

--- crowdfounding-notify/src/main/java/com/wayn/notify/config/WebSocketConfig.java
+++ crowdfounding-notify/src/main/java/com/wayn/notify/config/WebSocketConfig.java
@@ -1,10 +1,16 @@
 package com.wayn.notify.config;
 
+import com.wayn.notify.config.handshakehandler.MyPrincipalHandshakeHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 /**
  * websocket启用stomp消息配置
@@ -12,18 +18,56 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+    private static final Logger log = LoggerFactory.getLogger(WebSocketConfig.class);
+
+    @Autowired
+    private MyPrincipalHandshakeHandler myPrincipalHandshakeHandler;
+
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-        stompEndpointRegistry.addEndpoint("/notify");
-        stompEndpointRegistry.addEndpoint("/notify").setAllowedOrigins("*").withSockJS();
+        stompEndpointRegistry.setErrorHandler(new StompSubProtocolErrorHandler());
+        stompEndpointRegistry.addEndpoint("/notify")
+                // 配置拦截器，在拦截器中配置用户认证信息
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                // 配置握手处理器，websocket的用户认证信息从握手处理器获得
+                .setHandshakeHandler(myPrincipalHandshakeHandler)
+                // 配置websocket跨域
+                .setAllowedOrigins("*")
+                // 启用sockjs
+                .withSockJS()
+                // 设置连接时长
+                .setDisconnectDelay(30 * 1000);
     }
+
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/queue");
         registry.setUserDestinationPrefix("/user/");
         registry.setApplicationDestinationPrefixes("/app");//走@messageMapping
+//        registry.setPathMatcher(new AntPathMatcher()); // 设置路径匹配规则
     }
-    
+
+   // 通过configureClientInboundChannel方法进行用户认证处理
+    /*@Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        super.configureClientInboundChannel(registration);
+        registration.interceptors(new ChannelInterceptorAdapter() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    Principal user = new Principal() {
+                        @Override
+                        public String getName() {
+                            return ShiroUtil.getSessionUser().getId();
+                        }
+                    }; // access authentication header(s)
+                    accessor.setUser(user);
+                }
+                return message;
+            }
+        });
+    }*/
 }
--- crowdfounding-web/src/main/java/com/wayn/web/controller/profile/ProfileController.java
+++ crowdfounding-web/src/main/java/com/wayn/web/controller/profile/ProfileController.java
@@ -7,6 +7,9 @@ import com.wayn.commom.domain.User;
 import com.wayn.commom.domain.vo.UserResetPasswordVO;
 import com.wayn.commom.service.DeptService;
 import com.wayn.commom.service.UserService;
+import com.wayn.commom.util.FileUploadUtil;
+import com.wayn.commom.util.HttpUtil;
+import com.wayn.commom.util.ProperUtil;
 import com.wayn.commom.util.Response;
 import com.wayn.framework.annotation.RepeatSubmit;
 import org.apache.commons.lang3.StringUtils;
@@ -16,21 +19,25 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 个人资料处理控制器
+ */
 @Controller
 @RequestMapping("/profile")
 public class ProfileController extends BaseControlller {
 
-    private static final String PREFIX = "profile";
+    private static final String PREFIX = "profile" ;
 
     @Autowired
     private DeptService deptService;
@@ -50,36 +57,100 @@ public class ProfileController extends BaseControlller {
             deptNames.add(dept.getDeptName());
         }
         model.addAttribute("deptName", StringUtils.join(deptNames, " / "));
-        return PREFIX + "/profile";
+        return PREFIX + "/profile" ;
     }
 
+    @GetMapping("/avatar")
+    public String avatar(Model model) {
+        model.addAttribute("imgSrc", getCurUser().getUserImg());
+        return PREFIX + "/avatar" ;
+    }
+
+    /**
+     * 更新个人资料
+     *
+     * @param request
+     * @param user
+     * @return
+     */
     @RepeatSubmit
     @ResponseBody
     @PostMapping("updateUser")
-    public Response updateUser(Model model, User user) {
+    public Response updateUser(HttpServletRequest request, User user) {
+        // 更新用户信息后，浏览器端订阅的用户认证信息就不一致了，需要刷新浏览器。
+        // 如果想要一致，需要修改stompEndpointRegistry添加握手处理器，将用户认证消息替换成用户id。
+        ServletServerHttpRequest servletServerHttpRequest = new ServletServerHttpRequest(request);
+        servletServerHttpRequest.getPrincipal();
+
         userService.updateById(user);
         Subject subject = SecurityUtils.getSubject();
         String realmName = subject.getPrincipals().getRealmNames().iterator().next();
-        PrincipalCollection newPrincipalCollection =
-                new SimplePrincipalCollection(userService.selectById(user.getId()), realmName);
+        PrincipalCollection newPrincipalCollection = new SimplePrincipalCollection(userService.selectById(user.getId()), realmName);
+        // 重新加载Principal
         subject.runAs(newPrincipalCollection);
         return Response.success("修改用户信息成功！");
     }
 
+    /**
+     * 判断旧密码是否正确
+     *
+     * @param oldPassword
+     * @return
+     */
     @ResponseBody
-    @PostMapping("oldPasswordJudge")
-    public boolean oldPasswordJudge(Model model, String oldPassword) {
+    @PostMapping("judgeOldPasswordSuccess")
+    public boolean judgeOldPasswordSuccess(String oldPassword) {
         // 获取加密后的密码
         String password = new SimpleHash("MD5", oldPassword, userService.selectById(getCurUserId()).getUserName(), 1024).toString();
         List<User> users = userService.selectList(new EntityWrapper<User>().eq("password", password));
-        return users.size() > 0 ? true : false;
+        return users.size() > 0;
     }
 
     @ResponseBody
     @PostMapping("userResetPwd")
-    public Response userResetPwd(Model model, UserResetPasswordVO userResetPasswordVO) {
+    public Response userResetPwd(UserResetPasswordVO userResetPasswordVO) {
         String password = new SimpleHash("MD5", userResetPasswordVO.getNewPassword(), userService.selectById(getCurUserId()).getUserName(), 1024).toString();
         userService.updateForSet("password = '" + password + "'", new EntityWrapper<User>().eq("id", getCurUserId()));
         return Response.success("修改用户密码成功！");
     }
+
+
+    /**
+     * 更新头像照片
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    @ResponseBody
+    @PostMapping("updateAvatar")
+    public Response updateAvatar(@RequestParam("avatarfile") MultipartFile file) throws IOException {
+        // 上传文件路径
+        String filePath = ProperUtil.get("wayn.uploadDir") + "/avatar" ;
+        String fileName = FileUploadUtil.uploadFile(file, filePath);
+        // Thumbnails.of(filePath + "/" + fileName).size(64, 64).toFile(new File(filePath, fileName));
+        String requestUrl = HttpUtil.getRequestContext(request);
+        String url = requestUrl + "/upload/avatar/" + fileName;
+        userService.updateForSet("userImg = '" + url + "'", new EntityWrapper<User>().eq("id", getCurUserId()));
+        // 更新subject中用户信息
+        Subject subject = SecurityUtils.getSubject();
+        String realmName = subject.getPrincipals().getRealmNames().iterator().next();
+        PrincipalCollection newPrincipalCollection = new SimplePrincipalCollection(userService.selectById(getCurUserId()), realmName);
+        // 重新加载Principal
+        subject.runAs(newPrincipalCollection);
+        return Response.success("上传头像成功！");
+    }
+
+    /**
+     * 检查用户登陆是否过期，或者已经被登出
+     *
+     * @return Response
+     */
+    @ResponseBody
+    @PostMapping("auth")
+    public Response auth() {
+        getCurUser();
+        return Response.success();
+    }
+
 }
