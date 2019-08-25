6abb0eb8fec48c9aa36e6ab42c45eb7cb3f025ee
wayn <1669738430@qq.com>
2019-08-25 14:03:24 +0800

    优化websocket配置代码

---
+++ crowdfounding-notify/src/main/java/com/wayn/notify/mbean/MbeanManager.java
@@ -0,0 +1,28 @@
+package com.wayn.notify.mbean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class MbeanManager {
+
+    /**
+     * 导出WebSocketMessageBrokerStats信息·
+     * @param webSocketMessageBrokerStats
+     * @return
+     */
+    @Bean
+    public MBeanExporter mBeanExporter(WebSocketMessageBrokerStats webSocketMessageBrokerStats) {
+        MBeanExporter mBeanExporter = new MBeanExporter();
+        Map<String, Object> beans = new HashMap<>();
+        beans.put("webSocket:name=webSocketMessageBrokerStats", webSocketMessageBrokerStats);
+        mBeanExporter.setBeans(beans);
+        return mBeanExporter;
+    }
+
+}
