68406f20ce00537607fb429d939249df45d869db
wayn <1669738430@qq.com>
2019-09-27 16:23:40 +0800

    Revert "修复quartz模块bug"
    This reverts commit 4548a21a

--- crowdfounding-quartz/src/main/java/com/wayn/quartz/manager/task/WelComeJob.java
+++ crowdfounding-quartz/src/main/java/com/wayn/quartz/manager/task/WelComeJob.java
@@ -1,7 +1,9 @@
 package com.wayn.quartz.manager.task;
 
+import com.wayn.commom.util.SpringContextUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component("welComeJob")
@@ -10,7 +12,8 @@ public class WelComeJob {
     private static final Logger logger = LoggerFactory.getLogger(WelComeJob.class);
 
     public void welCome() {
-        System.out.println("WelCome to crowdfounfing！");
+        SimpMessagingTemplate simpMessagingTemplate = SpringContextUtil.getBean(SimpMessagingTemplate.class);
+        simpMessagingTemplate.convertAndSend("/topic/getResponse", "WelCome to crowdfounfing！");
         logger.info("执行自定义定时任务");
     }
 
