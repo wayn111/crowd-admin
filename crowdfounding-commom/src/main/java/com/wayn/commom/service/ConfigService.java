360f218af3e28a25a7e7b0c334ddbe7293c6c028
hequan_waynaqua <1669738430@qq.com>
2020-09-16 10:30:19 +0800

    fix(后台管理): 代码生成模块
    修复模板bug

---
+++ crowdfounding-commom/src/main/java/com/wayn/commom/service/ConfigService.java
@@ -0,0 +1,55 @@
+package com.wayn.commom.service;
+
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.service.IService;
+import com.wayn.commom.domain.Config;
+
+import java.util.List;
+
+/**
+ * 参数配置 服务层
+ *
+ * @author wayn
+ * @date 2020-09-16
+ */
+public interface ConfigService extends IService<Config> {
+    /**
+     * 查询参数配置信息
+     *
+     * @param page 分页对象，自动分页
+     * @return 参数配置信息
+     */
+    public Page<Config> selectConfigList(Page<Config> page, Config config);
+
+    /**
+     * 新增{tableComment}
+     *
+     * @param config 参数配置信息
+     * @return 结果
+     */
+    boolean save(Config config);
+
+    /**
+     * 修改参数配置
+     *
+     * @param config 参数配置信息
+     * @return 结果
+     */
+    boolean update(Config config);
+
+    /**
+     * 删除参数配置
+     *
+     * @param id
+     * @return 结果
+     */
+    boolean remove(Integer id);
+
+    /**
+     * 批量删除参数配置
+     *
+     * @param ids
+     * @return 结果
+     */
+    boolean batchRemove(Integer[] ids);
+}
