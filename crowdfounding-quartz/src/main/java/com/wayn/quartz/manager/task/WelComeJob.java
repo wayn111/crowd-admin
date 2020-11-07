ded72f48c8ed43117f4a235b518e5eec9ad0fe37
wayn <1669738430@qq.com>
2020-11-07 10:42:30 +0800

    perf(后台管理): 文件优化
    1、同步sql文件
    2、优化定时任务显示

--- crowdfounding-quartz/src/main/java/com/wayn/quartz/manager/task/WelComeJob.java
+++ crowdfounding-quartz/src/main/java/com/wayn/quartz/manager/task/WelComeJob.java
@@ -1,8 +1,10 @@
 package com.wayn.quartz.manager.task;
 
+import com.wayn.commom.service.ConfigService;
 import com.wayn.commom.util.SpringContextUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +13,13 @@ public class WelComeJob {
 
     private static final Logger logger = LoggerFactory.getLogger(WelComeJob.class);
 
+    @Autowired
+    private ConfigService configService;
+
     public void welCome() {
         SimpMessagingTemplate simpMessagingTemplate = SpringContextUtil.getBean(SimpMessagingTemplate.class);
-        simpMessagingTemplate.convertAndSend("/topic/getResponse", "WelCome to crowdfounfing！");
+        String sysName = configService.getValueByKey("sys.name");
+        simpMessagingTemplate.convertAndSend("/topic/getResponse", "WelCome to " + sysName + "！");
         logger.info("执行自定义定时任务");
     }
 
