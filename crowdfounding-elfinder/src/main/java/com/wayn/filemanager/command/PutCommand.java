89b4a6b3a2365e9ca9a99a592e439aec4a5c588d
hequan_waynaqua <1669738430@qq.com>
2020-11-20 10:52:43 +0800

    perf(后台管理): elfinder
    修复put命令写入图片bug

--- crowdfounding-elfinder/src/main/java/com/wayn/filemanager/command/PutCommand.java
+++ crowdfounding-elfinder/src/main/java/com/wayn/filemanager/command/PutCommand.java
@@ -5,18 +5,18 @@
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the Trustsystems Desenvolvimento de Sistemas, LTDA. nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -32,6 +32,7 @@
 package com.wayn.filemanager.command;
 
 import com.alibaba.fastjson.JSONObject;
+import com.wayn.commom.util.ImageUtil;
 import com.wayn.filemanager.constant.ElFinderConstants;
 import com.wayn.filemanager.service.ElfinderStorage;
 import com.wayn.filemanager.service.VolumeHandler;
@@ -43,14 +44,20 @@ import java.io.OutputStream;
 public class PutCommand extends AbstractJsonCommand implements ElfinderCommand {
 
     public static final String ENCODING = "utf-8";
+    public static final String IAMGE_BASE64_FLAG = "data:image/jpeg;base64,";
 
     @Override
     protected void execute(ElfinderStorage elfinderStorage, HttpServletRequest request, JSONObject json) throws Exception {
         final String target = request.getParameter(ElFinderConstants.ELFINDER_PARAMETER_TARGET);
-
         VolumeHandler file = findTarget(elfinderStorage, target);
         OutputStream os = file.openOutputStream();
-        IOUtils.write(request.getParameter(ElFinderConstants.ELFINDER_PARAMETER_CONTENT), os, ENCODING);
+        String content = request.getParameter(ElFinderConstants.ELFINDER_PARAMETER_CONTENT);
+        if (content.contains(IAMGE_BASE64_FLAG)) {
+            byte[] bytes = ImageUtil.decodeImageStr(content.substring(IAMGE_BASE64_FLAG.length()));
+            IOUtils.write(bytes, os);
+        } else {
+            IOUtils.write(content, os, ENCODING);
+        }
         os.close();
         json.put(ElFinderConstants.ELFINDER_JSON_RESPONSE_CHANGED, new Object[]{getTargetInfo(request, file)});
     }
