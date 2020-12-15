f61698a1a18dc4e552cf70aecd7becb0601e71fe
hequan_waynaqua <1669738430@qq.com>
2020-12-15 19:20:25 +0800

    fix(后台管理): 登陆
    修复登陆bug

--- crowdfounding-web/src/main/java/com/wayn/web/controller/home/LoginController.java
+++ crowdfounding-web/src/main/java/com/wayn/web/controller/home/LoginController.java
@@ -18,10 +18,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
@@ -56,7 +53,7 @@ public class LoginController extends BaseControlller {
 
     @ResponseBody
     @PostMapping("/doLogin")
-    public Response doLogin(String userName, String password, boolean rememberMe, String clientKaptcha) {
+    public Response doLogin(String userName, String password, @RequestParam(defaultValue = "false") Boolean rememberMe, String clientKaptcha) {
         String kaptcha = (String) SecurityUtils.getSubject().getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
         if (!StringUtils.equalsIgnoreCase(clientKaptcha, kaptcha)) {
             logininforService.addLog(userName, Constant.LOGIN_FAIL, "验证码错误");
