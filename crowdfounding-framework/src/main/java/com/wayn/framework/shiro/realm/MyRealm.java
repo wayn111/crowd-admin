315093b7c8c48f6652582bb33fdf0b0a27138883
wayn <1669738430@qq.com>
2020-12-19 03:12:34 +0800

    fix(后台管理): 任务列表显示优化，参数设置优化
    1、优化任务列表显示
    2、添加单一用户是否登出参数设置

--- crowdfounding-framework/src/main/java/com/wayn/framework/shiro/realm/MyRealm.java
+++ crowdfounding-framework/src/main/java/com/wayn/framework/shiro/realm/MyRealm.java
@@ -4,12 +4,7 @@ import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.wayn.commom.constant.Constant;
 import com.wayn.commom.domain.User;
 import com.wayn.commom.enums.StateEnum;
-import com.wayn.commom.exception.BusinessException;
-import com.wayn.commom.service.LogininforService;
-import com.wayn.commom.service.RoleMenuService;
-import com.wayn.commom.service.UserRoleService;
-import com.wayn.commom.service.UserService;
-import com.wayn.commom.shiro.util.ShiroUtil;
+import com.wayn.commom.service.*;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
@@ -23,7 +18,6 @@ import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.support.DefaultSubjectContext;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import java.util.Collection;
@@ -31,31 +25,20 @@ import java.util.Set;
 
 public class MyRealm extends AuthorizingRealm {
 
-
-    @Value("${wayn.singeUserAuth}")
-    private boolean singeUserAuth;
-
-    @Value("${wayn.singeKickoutBefore}")
-    private boolean singeKickoutBefore;
-
-    /**
-     * 用户服务
-     */
     @Autowired
     private UserService userService;
-    /**
-     * 用户角色服务
-     */
+
     @Autowired
     private UserRoleService userRoleService;
-    /**
-     * 角色菜单服务
-     */
+
     @Autowired
     private RoleMenuService roleMenuService;
 
     @Autowired
     private LogininforService logininforService;
+
+    @Autowired
+    private ConfigService configService;
 
     @Autowired
     private SessionDAO sessionDAO;
@@ -66,12 +49,13 @@ public class MyRealm extends AuthorizingRealm {
     /**
      * 密码加密 测试
      *
-     * @param args
+     * @param args 测试
      */
     public static void main(String[] args) {
         // MD5,"原密码","盐",加密次数
         String pwd = new SimpleHash("MD5", "123456", "admin", 1024).toString();
         System.out.println(pwd);
+        System.out.println(Boolean.valueOf("true"));
     }
 
     /**
@@ -95,7 +79,7 @@ public class MyRealm extends AuthorizingRealm {
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         UsernamePasswordToken token2 = (UsernamePasswordToken) token;
         String username = token2.getUsername();
-        String password = ShiroUtil.md5encrypt(new String(token2.getPassword()), username);
+        // String password = ShiroUtil.md5encrypt(new String(token2.getPassword()), username);
         User sysUser = userService.selectOne(new EntityWrapper<User>().eq("userName", username));
         if (sysUser == null) {
             logininforService.addLog(username, Constant.LOGIN_FAIL, "用户不存在");
@@ -109,32 +93,32 @@ public class MyRealm extends AuthorizingRealm {
         /*if (!sysUser.getPassword().equals(password)) {
             throw new IncorrectCredentialsException("账号或密码不正确");
         }*/
-        if (sysUser.getUserState() == StateEnum.DISABLE.getState()) {
+        if (sysUser.getUserState().equals(StateEnum.DISABLE.getState())) {
             logininforService.addLog(username, Constant.LOGIN_FAIL, "该用户已被锁定，请稍后再试");
             throw new LockedAccountException("该用户已被锁定，请稍后再试");
         }
         // 是否进行单一用户登陆处理
+        boolean singeUserAuth = Boolean.parseBoolean(configService.getValueByKey("sys.user.singeUserAuth"));
         if (singeUserAuth) {
             Collection<Session> activeSessions = sessionDAO.getActiveSessions();
             for (Session activeSession : activeSessions) {
-                if (activeSession.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY) == null) {
-                    continue;
-                } else {
+                if (activeSession.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY) != null) {
                     SimplePrincipalCollection principalCollection = (SimplePrincipalCollection) activeSession
                             .getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY);
                     Object primaryPrincipal = principalCollection.getPrimaryPrincipal();
                     User user = (User) primaryPrincipal;
-                    if (user.getUserName().equals(username) && singeKickoutBefore) {
-                        if (singeKickoutBefore) {
-                            Session session = sessionDAO.readSession(activeSession.getId());
-                            if (session != null) {
-                                session.stop();
-                                sessionDAO.delete(session);
-                                simpMessagingTemplate.convertAndSendToUser(user.getId(), "/queue/getResponse", "新消息：" + "该账号已在其他机器登陆！");
-                            }
-                        } else {
+                    if (user.getUserName().equals(username)) {
+                        // 单一用户登出逻辑，是否强制登出前一用户
+                        boolean singeKickoutBefore = Boolean.parseBoolean(configService.getValueByKey("sys.user.singeKickoutBefore"));
+                        if (!singeKickoutBefore) {
                             logininforService.addLog(username, Constant.LOGIN_FAIL, "该用户已登陆，请先登出！");
-                            throw new BusinessException("该用户已登陆，请先登出！");
+                            throw new AuthenticationException("该用户已登陆，请先登出！");
+                        }
+                        Session session = sessionDAO.readSession(activeSession.getId());
+                        if (session != null) {
+                            session.stop();
+                            sessionDAO.delete(session);
+                            simpMessagingTemplate.convertAndSendToUser(user.getId(), "/queue/getResponse", "新消息：" + "该账号已在其他机器登陆！");
                         }
                     }
                 }
@@ -142,8 +126,7 @@ public class MyRealm extends AuthorizingRealm {
         }
         // 盐值加密
         ByteSource byteSource = ByteSource.Util.bytes(username);
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(sysUser, sysUser.getPassword(), byteSource, getName());
-        return info;
+        return new SimpleAuthenticationInfo(sysUser, sysUser.getPassword(), byteSource, getName());
     }
 
 
@@ -151,6 +134,5 @@ public class MyRealm extends AuthorizingRealm {
         doClearCache(SecurityUtils.getSubject().getPrincipals());
         clearCachedAuthorizationInfo(SecurityUtils.getSubject().getPrincipals());
     }
-
 
 }
