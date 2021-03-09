0b4586839a9f2a10f52dd0730a3a388a7456f6e0
hequan_waynaqua <1669738430@qq.com>
2021-03-09 18:33:48 +0800

    perf(后台管理): 代码优化

--- crowdfounding-quartz/src/main/java/com/wayn/quartz/config/SchedulerConfig.java
+++ crowdfounding-quartz/src/main/java/com/wayn/quartz/config/SchedulerConfig.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 public class SchedulerConfig {
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(DataSource dataSource,AutowiringSpringBeanJobFactory jobFactory) {
+    public SchedulerFactoryBean schedulerFactoryBean(DataSource dataSource, AutowiringSpringBeanJobFactory jobFactory) {
         SchedulerFactoryBean factory = new SchedulerFactoryBean();
         factory.setDataSource(dataSource);
 
