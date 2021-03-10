07ad1008224aea615ad9e3c554ebd49fb1d2fc6d
hequan_waynaqua <1669738430@qq.com>
2021-03-10 19:34:48 +0800

    perf(后台管理): 优化代码

--- crowdfounding-quartz/src/main/java/com/wayn/quartz/config/AutowiringSpringBeanJobFactory.java
+++ crowdfounding-quartz/src/main/java/com/wayn/quartz/config/AutowiringSpringBeanJobFactory.java
@@ -7,9 +7,18 @@ import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 import org.springframework.stereotype.Component;
 
 
+/**
+ * <p>继承SpringBeanJobFactory接口获取Job实例，
+ * 并通过AutowireCapableBeanFactory
+ * 将Job实例交给Spring管理生命周期
+ */
 @Component
 public class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory {
 
+    /**
+     * 原对象可以不在Spring的IOC容器里，但是其属性成员需要被依赖注入，可以通过AutowireCapableBeanFactory
+     * 来实现属性的注入
+     */
     @Autowired
     private AutowireCapableBeanFactory beanFactory;
 
