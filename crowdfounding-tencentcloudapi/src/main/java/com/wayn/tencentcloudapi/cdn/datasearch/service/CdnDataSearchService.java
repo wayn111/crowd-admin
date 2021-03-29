9cbf489f6919338fb2d5b294a1d26bb6f34b115a
hequan_waynaqua <1669738430@qq.com>
2021-03-29 18:51:27 +0800

    bugfix

--- crowdfounding-tencentcloudapi/src/main/java/com/wayn/tencentcloudapi/cdn/datasearch/service/CdnDataSearchService.java
+++ crowdfounding-tencentcloudapi/src/main/java/com/wayn/tencentcloudapi/cdn/datasearch/service/CdnDataSearchService.java
@@ -2,16 +2,21 @@ package com.wayn.tencentcloudapi.cdn.datasearch.service;
 
 import com.tencentcloudapi.cdn.v20180606.CdnClient;
 
+/**
+ * cdn数据查询接口
+ */
 public interface CdnDataSearchService {
 
 
     CdnClient getCdnClient();
+
     /**
      * top数据查询
-     * @param startTime
-     * @param endTime
-     * @param metric
-     * @param filter
+     *
+     * @param startTime 开始时间
+     * @param endTime   结束时间
+     * @param metric    排序使用的指标名称
+     * @param filter    Get 排序使用的指标名称
      * @param <T>
      * @return
      */
@@ -19,9 +24,10 @@ public interface CdnDataSearchService {
 
     /**
      * 访问数据查询
-     * @param startTime
-     * @param endTime
-     * @param metric
+     *
+     * @param startTime 开始时间
+     * @param endTime   结束时间
+     * @param metric    排序使用的指标名称
      * @param <T>
      * @return
      */
