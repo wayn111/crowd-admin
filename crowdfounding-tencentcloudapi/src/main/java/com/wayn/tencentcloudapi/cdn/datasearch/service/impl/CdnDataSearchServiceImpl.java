d21938ee3112524a9d9f68cd24dc3f73feb07b90
wayn <1669738430@qq.com>
2021-05-30 19:27:09 +0800

    feat(pom): 代码优化

--- crowdfounding-tencentcloudapi/src/main/java/com/wayn/tencentcloudapi/cdn/datasearch/service/impl/CdnDataSearchServiceImpl.java
+++ crowdfounding-tencentcloudapi/src/main/java/com/wayn/tencentcloudapi/cdn/datasearch/service/impl/CdnDataSearchServiceImpl.java
@@ -3,12 +3,8 @@ package com.wayn.tencentcloudapi.cdn.datasearch.service.impl;
 import com.tencentcloudapi.cdn.v20180606.CdnClient;
 import com.tencentcloudapi.cdn.v20180606.models.DescribeCdnDataRequest;
 import com.tencentcloudapi.cdn.v20180606.models.ListTopDataRequest;
-import com.tencentcloudapi.common.Credential;
 import com.tencentcloudapi.common.exception.TencentCloudSDKException;
-import com.tencentcloudapi.common.profile.ClientProfile;
-import com.tencentcloudapi.common.profile.HttpProfile;
 import com.wayn.tencentcloudapi.cdn.datasearch.service.CdnDataSearchService;
-import com.wayn.tencentcloudapi.config.CredentialConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +21,12 @@ public class CdnDataSearchServiceImpl implements CdnDataSearchService {
     @Override
     public <T> T topDataSearch(String startTime, String endTime, String metric, String filter) {
         try {
-            CdnClient client = cdnClient;
-
             ListTopDataRequest req = new ListTopDataRequest();
             req.setStartTime("2021-03-22 00:00:00");
             req.setEndTime("2021-03-28 15:00:00");
             req.setMetric(metric);
             req.setFilter(filter);
-            return (T) client.ListTopData(req);
+            return (T) cdnClient.ListTopData(req);
         } catch (TencentCloudSDKException e) {
             logger.error(e.getMessage(), e);
         }
@@ -42,16 +36,11 @@ public class CdnDataSearchServiceImpl implements CdnDataSearchService {
     @Override
     public <T> T accessDataSearch(String startTime, String endTime, String metric) {
         try {
-
-            CdnClient client = cdnClient;
-
             DescribeCdnDataRequest req = new DescribeCdnDataRequest();
             req.setStartTime(startTime);
             req.setEndTime(endTime);
             req.setMetric(metric);
-
-            return (T) client.DescribeCdnData(req);
-
+            return (T) cdnClient.DescribeCdnData(req);
         } catch (TencentCloudSDKException e) {
             logger.error(e.getMessage(), e);
         }
