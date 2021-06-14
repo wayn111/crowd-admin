39202202b4190010698f5b48cb22df4c7ce1395c
wayn <1669738430@qq.com>
2021-06-14 19:03:13 +0800

    feat(crowd): 版本升级
    升级至2.3版本，优化了sql文件以及代码目录，修复bug漏洞

--- crowd-commom/src/main/java/com/wayn/commom/util/FileUtils.java
+++ crowd-commom/src/main/java/com/wayn/commom/util/FileUtils.java
@@ -26,7 +26,6 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
      *
      * @param filePath 文件路径
      * @param os       输出流
-     * @return
      */
     public static void writeBytes(String filePath, OutputStream os) throws IOException {
         FileInputStream fis = null;
@@ -65,7 +64,7 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
      * 删除文件
      *
      * @param filePath 文件
-     * @return
+     * @return boolean
      */
     public static boolean deleteFile(String filePath) {
         boolean flag = false;
@@ -101,12 +100,7 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
         }
 
         // 检查允许下载的文件规则
-        if (ArrayUtils.contains(MimeTypeUtils.DEFAULT_ALLOWED_EXTENSION, FilenameUtils.getExtension(resource))) {
-            return true;
-        }
-
-        // 不在允许下载的文件规则
-        return false;
+        return ArrayUtils.contains(MimeTypeUtils.DEFAULT_ALLOWED_EXTENSION, FilenameUtils.getExtension(resource));
     }
 
 
@@ -149,7 +143,6 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
      *
      * @param response     响应对象
      * @param realFileName 真实文件名
-     * @return
      */
     public static void setAttachmentResponseHeader(HttpServletResponse response, String realFileName) throws UnsupportedEncodingException {
         String percentEncodedFileName = percentEncode(realFileName);
